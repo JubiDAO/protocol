a3c7b4a97bd70b1f42c679bf3cea55a822048215
Jeeva Suresh <jeevas@saftely.io>
2022-05-25 15:17:19 +1000

    Updated local deploy to create a few test invite codes

--- scripts/deploys/local.ts
+++ scripts/deploys/local.ts
@@ -2,6 +2,8 @@ import '@nomiclabs/hardhat-ethers';
 import { ethers } from 'hardhat';
 import { FakeERC20__factory, Presale__factory  } from '../../typechain';
 import { DeployedContracts } from './contract-addresses';
+import { MerkleTree } from 'merkletreejs';
+import keccak256 from 'keccak256';
 
 import { blockTimestamp, toAtto } from '../../shared/utils';
 
@@ -14,7 +16,24 @@ async function main() {
   const fakeUSD = await new FakeERC20__factory(owner).deploy("USD", "USD");
   const fakeSaftelyToken = await new FakeERC20__factory(owner).deploy("Saftley", "SAFTELY");
 
+  const inviteCodes = [
+    "asdf12", 
+    "123asdf", 
+    "SLKJ3n1", 
+    "098123AS", 
+    "09lkmqwe", 
+    "S3JOInsa",
+    "1auwwizf",
+    "1960nkp7",
+    "t9x1gWXF",
+    "CLEQudf8l3",
+    "16a1v5vUug",
+    "nswe90XVqy"
+  ];
+  const inviteMerkleTree = new MerkleTree(inviteCodes.map(c => keccak256(c)), keccak256, {sortPairs: true});
+
   const presale = await new Presale__factory(owner).deploy(
+    inviteMerkleTree.getRoot(),
     await blockTimestamp() + 10000,
     await blockTimestamp(),
     100000,
@@ -41,6 +60,11 @@ async function main() {
     console.log(`${envvar}=${contractAddressAsMapping[envvar]}`);
   }
   console.log('=========================================');
+
+  console.log();
+  console.log();
+  console.log('*** INVITE CODES ***');
+  inviteCodes.forEach(c => console.log(c));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
